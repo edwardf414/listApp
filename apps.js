2013fae8042963ddcedff3d7597a2a5c16930370
Edward Fernandez <edwardf414@gmail.com>
2022-08-08 23:46:17 -0400

    Templating & Partials

--- apps.js
+++ apps.js
@@ -1,15 +1,58 @@
 const express = require('express'); 
+const { allowedNodeEnvironmentFlags } = require('process');
 const app = express(); 
 
-app.get('/', (req, res) =>{
-    var today = new Date();
-    var currentDay = today.getDate(); 
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
-    if(currentDay === 6 || currentDay === 0){
-        res.send("Its the weekend baybay");
+let newListItems =["Buy food", "Cook Food", "Eat Food"]; 
+let workitems = [];
+
+app.get('/', (req, res) =>{
+
+    let options = {
+        weekday :"long", 
+        day : "numeric", 
+        month : "long"
+    }; 
+
+    let today = new Date();
+
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render('list', {listType: day, newListItems : newListItems});
+}); 
+
+app.get("/work", (req, res) =>{
+    res.render('list', { listType: "Work List", newListItems : workitems});
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
+})
+
+app.post("/", (req, res) => {
+
+    console.log(req.body.list);
+    let newListItem = req.body.newName;
+    
+    if (req.body.list === "Work"){
+        workitems.push(newListItem); 
+        res.redirect("/work");
     }else{
-        res.send("snap I got to work");
+        newListItems.push(newListItem);
+        res.redirect("/");
     }
+
+
+});
+
+app.post("/work", (req, res) => {
+    let workitem = req.body.newListItem; 
+    newListItems.push(workitem); 
+    res.redirect("/work");
 })
 
 app.listen(3000, () =>{
